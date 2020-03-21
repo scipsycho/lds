b17718eae683b59f176f1dd58a0b8e925e06eb56
Dharmesh Singh <dsingh3@atlassian.com>
2020-03-21 17:36:05 +0530

    Suggestions in bash completion are faster now
    This is because some of those variables are now stored in global bash variables and will refresh only when a new session is restarted and the function is invoked

--- bin/goto_bash_completion.sh
+++ bin/goto_bash_completion.sh
@@ -1,13 +1,16 @@
 function _goto() {
-  local cur prev opts __lds_goto_completion_commands __lds_goto_completion_aliases __lds_goto_completion_shortOptions __lds_goto_completion_longOptions
+  local cur prev opts __lds_goto_completion_dictionary
   COMPREPLY=()
   cur="${COMP_WORDS[COMP_CWORD]}"
   prev="${COMP_WORDS[COMP_CWORD-1]}"
 
+  if [[ -z "$__lds_goto_completion_commands" ]]
+  then
   __lds_goto_completion_commands=`goto help | grep -A 500 "Commands:" | grep -B 500 "Options:" | grep -v "\(Options\|Commands\):" | awk -F ":" '{ print $1 }' | tr "\n" " "`
   __lds_goto_completion_aliases=`cat $LDS_GOTO_FILE  | sed "s/local LDS_ALIAS_//g" | awk -F "=" '{ print $1 }' | tr "\n" " "`
   __lds_goto_completion_shortOptions=`goto help | ggrep -o -P '(?<!-)-\w+'  | tr "\n" " "`
   __lds_goto_completion_longOptions=`goto help | ggrep -o -P '\-\-\w+'  | tr "\n" " "`
+  fi
   
   __lds_goto_completion_dictionary=""
   if [[ ${prev} == "goto" ]]
