afbf8ee7c5b206773be4d5e4ff39f6c433b1579e
Dharmesh Singh <dsingh3@atlassian.com>
2020-03-21 17:28:04 +0530

    removed some bugs

--- bin/goto_bash_completion.sh
+++ bin/goto_bash_completion.sh
@@ -1,32 +1,32 @@
-_goto() {
-  local cur prev opts
+function _goto() {
+  local cur prev opts __lds_goto_completion_commands __lds_goto_completion_aliases __lds_goto_completion_shortOptions __lds_goto_completion_longOptions
   COMPREPLY=()
   cur="${COMP_WORDS[COMP_CWORD]}"
   prev="${COMP_WORDS[COMP_CWORD-1]}"
 
-  commands=`goto help | grep -A 500 "Commands:" | grep -B 500 "Options:" | grep -v "\(Options\|Commands\):" | awk -F ":" '{ print $1 }' | tr "\n" " "`
-  aliases=`cat $LDS_GOTO_FILE  | sed "s/local LDS_ALIAS_//g" | awk -F "=" '{ print $1 }' | tr "\n" " "`
-  shortOptions=`goto help | ggrep -o -P '(?<!-)-\w+'  | tr "\n" " "`
-  longOptions=`goto help | ggrep -o -P '\-\-\w+'  | tr "\n" " "`
+  __lds_goto_completion_commands=`goto help | grep -A 500 "Commands:" | grep -B 500 "Options:" | grep -v "\(Options\|Commands\):" | awk -F ":" '{ print $1 }' | tr "\n" " "`
+  __lds_goto_completion_aliases=`cat $LDS_GOTO_FILE  | sed "s/local LDS_ALIAS_//g" | awk -F "=" '{ print $1 }' | tr "\n" " "`
+  __lds_goto_completion_shortOptions=`goto help | ggrep -o -P '(?<!-)-\w+'  | tr "\n" " "`
+  __lds_goto_completion_longOptions=`goto help | ggrep -o -P '\-\-\w+'  | tr "\n" " "`
   
-  dictionary=""
+  __lds_goto_completion_dictionary=""
   if [[ ${prev} == "goto" ]]
   then
     if [[ "${cur}" =~ --[a-z]* ]]
     then
-      dictionary="${longOptions}"
+      __lds_goto_completion_dictionary="${__lds_goto_completion_longOptions}"
     elif [[ "${cur}" =~ -[a-z]* ]]
     then
-      dictionary="${shortOptions}"
+      __lds_goto_completion_dictionary="${__lds_goto_completion_shortOptions}"
     elif [[ "${cur}" =~ [a-z]* ]] 
     then
-      dictionary="${commands} ${aliases}"
+      __lds_goto_completion_dictionary="${__lds_goto_completion_commands} ${__lds_goto_completion_aliases}"
     fi
   elif [[ ${prev} == "-d" ]] || [[ ${prev} == "--deregister" ]]
   then
-   dictionary="${aliases}" 
+   __lds_goto_completion_dictionary="${__lds_goto_completion_aliases}" 
   fi
-    COMPREPLY=( `compgen -W "${dictionary}" -- ${cur}` )
+    COMPREPLY=( `compgen -W "${__lds_goto_completion_dictionary}" -- ${cur}` )
     return 0
 }
 
